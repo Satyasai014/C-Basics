95f788cce74d5e844b2b221b7d01be810d09c1e9
Satyasai014 <92287752+Satyasai014@users.noreply.github.com>
2021-12-08 09:56:24 +0530

    Add files via upload

---
+++ marks.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("enter your marks to learn your grade");
+	scanf("%d",&n);
+	if(n >= 80)
+	{
+		printf("your grade for this subject is A");
+	}
+	else if(n = 80 < 70);
+	{
+		printf("ypur grade for this subject is B");
+	}
+	if(n = 50 >= 59);
+	{
+		printf("your grade for this subject is C");
+	}
+	if(n = 40 >=49)
+	{
+		printf("your grade for this subject is D");
+	}
+}
