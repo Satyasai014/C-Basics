d4a832a6e7b86144b4d6ca69c278cf54ef54d9b5
Satyasai014 <92287752+Satyasai014@users.noreply.github.com>
2021-12-08 10:15:26 +0530

    Add files via upload

---
+++ logical divisibility rules.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("enter any number to check multiples\n");
+	scanf("%d",&n);
+	if(n%1 == 0)
+	{
+		printf("the given number is divisible by 1\n");
+	}
+	if(n%2 == 0)
+	{
+		printf("the given number is divisible by 2\n");
+	}
+	if(n%3 == 0)
+	{
+		printf("the given number is divisible by 3\n");
+	}
+	if(n%4 == 0)
+	{
+		printf("the given number is divisible by 4\n");
+	}
+	
+	if(n%5 == 0)
+	{
+		printf("the given number is divisible by 5\n");
+	}
+	if(n%6 == 0)
+	{
+		printf("the given number is divisible by 6\n");
+	}
+	if(n%7 == 0)
+	{
+		printf("the given number is divisible by 7\n");
+	}
+	if(n%8 == 0)
+	{
+		printf("the given number is divisible by 8\n");
+	}
+	if(n%9 == 0)
+	{
+		printf("the given number is divisible by 9\n");
+	}
+	if(n%10 == 0)
+	{
+		printf("the given number is divisible by 10 \n");
+	}
+}
